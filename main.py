c0d13545244b5046b4069fe1014b73ac0e3b46dd
gghudulov <113235709+gghudulov@users.noreply.github.com>
2022-11-28 11:23:13 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+maxn = 10
+n = random.randint(1, maxn)
+print('Welcome to guess the number game!')
+print('Guess the number from 1 to %d' % maxn)
+guess = None
+while guess != n:
+    guess = int(input('Your try: '))
+    if guess > n:
+        print('Too high')
+    if guess < n:
+        print('Too low')
+
+print('Congratulations, you won!')
+print('maded by gg.hudulov')
+print('Website: www.hudulov.cf')
